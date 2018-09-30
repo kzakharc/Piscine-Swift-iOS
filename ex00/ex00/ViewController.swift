ff92f626a2c40e58eff406d4e16b4f9573620ec0
iriska <iriska@inernetbs.net>
2018-09-30 23:42:55 +0300

    Initial commit Day00

---
+++ ex00/ex00/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ex00
+//
+//  Created by Kateryna Zakharchuk on 9/30/18.
+//  Copyright © 2018 Kateryna Zakharchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func touchClickMe(_ sender: UIButton) {
+        print("Hello World !")
+    }
+}
+
---
+++ ex01/ex01/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ex01
+//
+//  Created by Kateryna Zakharchuk on 9/30/18.
+//  Copyright © 2018 Kateryna Zakharchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var helloWordLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func touchClickMeButton(_ sender: UIButton) {
+        helloWordLabel.text = "Hello World !"
+    }
+}
+

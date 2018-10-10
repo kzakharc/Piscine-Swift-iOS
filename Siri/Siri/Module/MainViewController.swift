45b83701b44362d07f013683c1270b614261b632
kzakharc <kzakharc@student.unit.ua>
2018-10-10 23:11:22 +0300

    add tmp version of Siri

---
+++ Siri/Siri/Module/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  Siri
+//
+//  Created by Kateryna Zakharchuk on 10/10/18.
+//  Copyright © 2018 Kateryna Zakharchuk. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ Siri/Siri/NetworkManager.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkManager.swift
+//  Siri
+//
+//  Created by Kateryna Zakharchuk on 10/10/18.
+//  Copyright © 2018 Kateryna Zakharchuk. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    private let darkSkySecretKey = "06f65ccd16c98c7b81d4a545e608a437"
+    private let darkSkyLink  = "https://api.darksky.net/forecast/06f65ccd16c98c7b81d4a545e608a437/37.8267,-122.4233"
+}
